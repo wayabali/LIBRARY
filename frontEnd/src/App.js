d5d80cabb2a3a6a807899d8701eeed491070c07d
wayabali <ryadbali60@gmail.com>
2024-05-01 10:59:55 +0100

    final

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import SignupComponent from './Components/Signup/SignupComponent';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginComponent from './Components/Login/Logincomponent';
-import Quiz from './Components/quizz/quizz';
-import questionsData from './Components/quizz/quizz.json';
 
 
 
@@ -14,7 +12,6 @@ function App() {
         <Routes>
           <Route  path="/signup" element={<SignupComponent />} />
           <Route path="/login" element={<LoginComponent />} />
-          <Route path="/qui" element={<Quiz />} />
         </Routes>
       </div>
     </Router>
